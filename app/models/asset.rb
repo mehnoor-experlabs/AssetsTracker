87a48b541fdf68fab68505a0a2d321433ceb94ac
mehnoor-experlabs <113883842+mehnoor-experlabs@users.noreply.github.com>
2022-10-04 13:20:12 +0500

    asset name and password should be present

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,4 +2,5 @@ class Asset < ApplicationRecord
     belongs_to :user
     has_many :credentials, as: :credentiable
     has_many :comments, dependent: :destroy
+    validates :name, :password, presence: true
 end
